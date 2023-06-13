ecda9d4f6d26e3f1082dcf90d69305e8b3dbbe27
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-06-13 16:04:08 +0300

    commit

---
+++ app/core/app.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace MVC\core;
+
+class app
+{
+  public function __construct()
+  {
+  }
+  private $controller;
+  private $method;
+  private $params;
+  private function url()
+  {
+    if (!empty($_SERVER['QUERY_STRING'])) {
+      $url = explode('/', $_SERVER['QUERY_STRING']);
+      $this->controller = !empty($url[0]) ? $url[0] . 'controller' : 'index';
+      $this->method = !empty($url[1]) ? $url[1] . 'controller' : 'index';
+      unset($url[0], $url[1]);
+      $this->params = array_values($url);
+    } else {
+      echo 'EMPTY QUERY STRING';
+    }
+  }
+  private function render()
+  {
+    $controller = 'MVC\app\\' . $this->controller;
+    if (class_exists($controller)) {
+      $controller = new $controller;
+      if (method_exists($controller, $this->method)) {
+        call_user_func_array([$controller, $this->method], $this->params);
+      } else {
+        echo 'METHOD NOT EXISTS';
+      }
+    } else {
+      echo 'CLASS NOT EXISTS';
+    }
+  }
+}
