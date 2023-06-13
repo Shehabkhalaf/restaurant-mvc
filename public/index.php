df1e59df23fd4d163df01f4c4bc77b29e5f76aa3
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-06-13 16:09:04 +0300

    change

--- public/index.php
+++ public/index.php
@@ -1,2 +1,14 @@
 <?php
-  
+
+use MVC\core\app;
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__DIR__) . DS);
+define('APP', ROOT . 'app' . DS);
+define('controllers', APP . 'controllers' . DS);
+define('CORE', APP . 'core' . DS);
+define('MODELS', APP . 'models' . DS);
+define('VIEWS', APP . 'views' . DS);
+
+require_once('../vendor/autoload.php');
+$app = new app;
